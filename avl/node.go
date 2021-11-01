43c14c7c2da8e31e8a880cb61feb6010260a5627
cmtudor <31376942+chancetudor@users.noreply.github.com>
2021-10-31 20:27:32 -0400

    edited node.go for AVL node

--- avl/node.go
+++ avl/node.go
@@ -1,1 +1,259 @@
 package avl
+
+import (
+	"fmt"
+	"github.com/emirpasic/gods/utils"
+)
+
+// Node stores left, right, and parent Node pointers; the node's color,
+// and NodeData, containing the key and the value the caller wishes to store.
+type Node struct {
+	left   *Node
+	right  *Node
+	parent *Node
+	Data   *NodeData
+	height int
+}
+
+// NodeData stores the key and the value of the Node.
+type NodeData struct {
+	Key   interface{}
+	Value interface{}
+}
+
+// NewNode takes in a key and a value and returns a pointer to type Node.
+// When creating a new node, the left and right children, as well as the parent node, are set to nil.
+func NewNode(k, v interface{}, h int) *Node {
+	return &Node{
+		left:   nil,
+		right:  nil,
+		parent: nil,
+		Data: &NodeData{
+			Key:   k,
+			Value: v,
+		},
+		height: h,
+	}
+}
+
+// dfs traverses the nodes in a depth-first search paradigm.
+// The function prints by converting each node's key and value to a string.
+func (node *Node) dfs() {
+	if node == nil {
+		return
+	}
+	if node.isRoot() {
+		fmt.Println("ROOT")
+	}
+	fmt.Println("Key = " + utils.ToString(node.Data.Key) +
+		" | " + "Value = " + utils.ToString(node.Data.Value))
+	node.leftChild().dfs()
+	node.rightChild().dfs()
+}
+
+// inOrder traverses the nodes "in order," printing every node's value in order from smallest to greatest.
+// The function prints by converting each node's key and value to a string.
+func (node *Node) inOrder() {
+	if node == nil {
+		return
+	}
+	node.leftChild().inOrder()
+	if node.isRoot() {
+		fmt.Println("ROOT")
+	}
+	fmt.Println("Key = " + utils.ToString(node.Data.Key) +
+		" | " + "Value = " + utils.ToString(node.Data.Value))
+	node.rightChild().inOrder()
+}
+
+// isRoot checks to see if Node's parent is nil and if its color is black.
+// If both are true, the function returns true, as the Node is the tree's root.
+// Otherwise, the function returns false.
+func (node *Node) isRoot() bool {
+	if node.getParent() == nil {
+		return true
+	}
+
+	return false
+}
+
+// isLeaf checks to see if Node is a leaf.
+// If the Node's left Node and right Node are nil, the function returns true.
+// Otherwise, the function returns false.
+func (node *Node) isLeaf() bool {
+	if node.leftChild() == nil && node.rightChild() == nil {
+		return true
+	}
+
+	return false
+}
+
+// setLeftChild sets a Node's left child
+func (node *Node) setLeftChild(left *Node) {
+	if node != nil {
+		node.left = left
+	}
+}
+
+// leftChild returns the Node's left child.
+func (node *Node) leftChild() *Node {
+	if node != nil {
+		return node.left
+	}
+
+	return nil
+}
+
+// setRight sets a Node's right child
+func (node *Node) setRightChild(right *Node) {
+	if node != nil {
+		node.right = right
+	}
+}
+
+// rightChild returns the Node's right child.
+func (node *Node) rightChild() *Node {
+	if node != nil {
+		return node.right
+	}
+
+	return nil
+}
+
+// setParent sets a node's parent.
+func (node *Node) setParent(parent *Node) {
+	if node != nil {
+		node.parent = parent
+	}
+	// node.parent = parent
+}
+
+// getParent returns a node's parent.
+func (node *Node) getParent() *Node {
+	if node != nil {
+		return node.parent
+	}
+
+	return nil
+}
+
+// setGrandparent sets a node's grandparent.
+func (node *Node) setGrandparent(grandparent *Node) {
+	if node != nil {
+		node.parent.parent = grandparent
+	}
+	// node.parent.parent = grandparent
+}
+
+// grandparent returns a node's grandparent.
+func (node *Node) grandparent() *Node {
+	if node != nil {
+		return node.parent.parent
+	}
+
+	return nil
+}
+
+// uncle returns a node's uncle.
+func (node *Node) uncle() *Node {
+	// return grandparent's right child
+	if node.grandparent().leftChild() == node.getParent() {
+		return node.grandparent().rightChild()
+	}
+	// else return grandparent's left child
+	return node.grandparent().leftChild()
+}
+
+// clear marks a node's parent and children as nil, effectively severing it from the tree.
+func (node *Node) clear() {
+	node.setParent(nil)
+	node.setLeftChild(nil)
+	node.setRightChild(nil)
+}
+
+// setKey takes a key and sets it as the key for a node.
+func (node *Node) setKey(key interface{}) {
+	if node != nil {
+		node.Data.Key = key
+	}
+}
+
+// key returns the value of a node.
+func (node *Node) key() interface{} {
+	if node != nil {
+		return node.Data.Key
+	}
+
+	return nil
+}
+
+// setValue takes a value and sets it as the value for a node.
+func (node *Node) setValue(value interface{}) {
+	if node != nil {
+		node.Data.Value = value
+	}
+}
+
+// value returns the value of a node.
+func (node *Node) value() interface{} {
+	if node != nil {
+		return node.Data.Value
+	}
+
+	return nil
+}
+
+// successor returns the node with the smallest key greater than the node the method is called on
+func (node *Node) successor() *Node {
+	// successor is the furthest left child of the right subtree
+	if node.rightChild() != nil {
+		// return node.subtreeMin(node.rightChild())
+		return node.rightChild().subtreeMin()
+	}
+	// otherwise, work up and to the right of the subtrees
+	parent := node.getParent()
+	temp := node
+	for parent != nil && temp == parent.rightChild() {
+		temp = parent
+		parent = parent.getParent()
+	}
+
+	return parent
+}
+
+// predecessor returns the node with the largest key smaller than the node the method is called on
+func (node *Node) predecessor() *Node {
+	// successor is the furthest left child of the right subtree
+	if node.leftChild() != nil {
+		return node.subtreeMax(node.rightChild())
+	}
+	// otherwise, work up and to the right of the subtrees
+	parent := node.getParent()
+	temp := node
+	for parent != nil && temp == parent.leftChild() {
+		temp = parent
+		parent = parent.getParent()
+	}
+
+	return parent
+}
+
+// subtreeMin returns the furthest left child of a subtree
+func (node *Node) subtreeMin( /*child *Node*/ ) *Node {
+	temp := node
+	for temp.leftChild() != nil {
+		temp = temp.leftChild()
+	}
+
+	return temp
+}
+
+// subtreeMax returns the furthest right child of a subtree
+func (node *Node) subtreeMax(child *Node) *Node {
+	temp := child
+	for temp.rightChild() != nil {
+		temp = temp.rightChild()
+	}
+
+	return temp
+}
